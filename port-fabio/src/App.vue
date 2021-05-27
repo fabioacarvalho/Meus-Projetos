fc3bce36c06ad24dbbcd3d48c1c0daa9e1b80d18
Fábio Carvalho <fabioxcarvalho@outlook.com>
2021-05-27 15:59:35 -0300

    atualizando

---
+++ port-fabio/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Header />
+    <Capa />
+    <Abstract />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Capa from './components/Capa.vue'
+import Abstract from './components/Abstract.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Capa,
+    Abstract
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ port-fabio/src/components/Capa.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <b-img src="../assets/img/capa.png" fluid alt="Capa png">
+        
+        </b-img>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
