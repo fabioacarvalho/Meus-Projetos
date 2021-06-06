80bfb2d0d30123de5b79a59960ab2122c8ac3711
Fábio Carvalho <fabioxcarvalho@outlook.com>
2021-06-06 17:58:41 -0300

    Uptdate

---
+++ site-pedidos-v1/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {  },
+  created() {
+        this.$http.post('db.json', {
+            nome: 'Maria',
+            email: 'maria@gmail.com'
+        }).then(res => console.log(res))
+    }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
